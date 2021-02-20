20442bee0c4b35ad9bfd8b06e4d0608cd895db99
Abdirs-code <warya1998@outlook.com>
2021-02-20 09:09:29 +0100

    added new features and deplyed the app

--- src/Banner.js
+++ src/Banner.js
@@ -6,7 +6,7 @@ import requests from "./Requests";
 function Banner() {
 	const [movie, setMovie] = useState([]);
 
-  useEffect(() => {
+	useEffect(() => {
 		async function fetchData() {
 			const request = await axios.get(requests.fetchNetflixOriginals);
 			setMovie(
@@ -18,8 +18,7 @@ function Banner() {
 		}
 		fetchData();
 	}, []);
-  //console.log(movie)
-
+	//console.log(movie)
 
 	const truncate = (string, n) => {
 		return string?.length > n ? string.substring(0, n - 1) + "..." : string;
@@ -30,22 +29,24 @@ function Banner() {
 			className="banner"
 			style={{
 				backgroundSize: "cover",
-				backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
 				backgroundPosition: "center center",
 			}}
 		>
 			<div className="banner__contents">
-				<h1 className="banner__title"> {movie?.title || movie?.name || movie?.original_name} </h1>
+				<h1 className="banner__title">
+					{" "}
+					{movie?.title || movie?.name || movie?.original_name}{" "}
+				</h1>
 				<div className="banner__buttons">
 					<button className="banner__button">Play</button>
 					<button className="banner__button">My List</button>
 				</div>
 				<h1 className="banner__description">
-					{truncate( movie?.overview, 160
-					)}
+					{truncate(movie?.overview, 160)}
 				</h1>
 			</div>
- 
+
 			<div className="banner--fadeButton" />
 		</header>
 	);
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -4,31 +4,40 @@ import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
 import Nav from "../Nav";
 import "./ProfileScreen.css";
+import PlansScreen from "./PlansScreen";
 
 
 function ProfileScreen() {
 	const user = useSelector(selectUser);
 	return (
-		<div className="profileScreen">
-			<Nav />
-			<div className="profileScreen__body">
-				<h1>Edit Profile</h1>
-				<div className="profileScreen__info">
-					<img
-						src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-						alt=""
-					/>
-					<div className="profileScreen__details">
-						<h2>{user.email} </h2>
-						<div className="profileScreen__plans">
-              <h3>Plans </h3>
-              <p></p>
-							<button onClick={() => auth.signOut()} className="profileScreen__signOut">Sign Out</button>
+		<form onSubmit={(e) => {e.preventDefault()}}>
+			<div className="profileScreen">
+				<Nav />
+				<div className="profileScreen__body">
+					<h1>Edit Profile</h1>
+					<div className="profileScreen__info">
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+							alt=""
+						/>
+						<div className="profileScreen__details">
+							<h2>{user.email} </h2>
+							<div className="profileScreen__plans">
+								<h3>Plans</h3>
+
+								{<PlansScreen />}
+								<button
+									onClick={() => auth.signOut()}
+									className="profileScreen__signOut"
+								>
+									Sign Out
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</form>
 	);
 }
 
